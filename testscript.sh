363cbaa510ca216a525f36b102207e1cfbfdd96d
mfaiz0 <mfaiz30@gmail.com>
2018-12-28 03:22:59 -0500

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -5,4 +5,4 @@ echo "That is running in a build step"
 echo "This is a param $PASSED"
 NUM=$(($FIRSTNUMBER + $SECONDNUMBER))
 echo "$NUM"
-echo "This is complete"
+echo "This is running automatically"
